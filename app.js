9f830b973a7845ad4c2cd76526d66e44ab42f1e7
vaibhav <vaibhav7290119@gmail.com>
2026-01-10 23:59:15 +0530

    added some featurs like view and delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const userModel=require("./models/user");
 
 app.set("view engine","ejs");
 app.use(express.json());
@@ -13,9 +14,32 @@ app.get("/",(req,res)=>{
 res.render("index.ejs")
 });
 
+//reading form data
+app.get("/read",async (req,res)=>{
+  let alluser=await userModel.find({});
+  res.render("read.ejs",{user:alluser})
+}
+)
+//creating a user
+app.post("/create",async (req,res)=>{
+  let {name,email,image}=req.body;
+  let createduser=await userModel.create({
+    name,
+    email,
+    image
+  });
+  res.redirect("/read")
 
+})
 
+//deleting a user
+app.get("/delete/:id",async (req,res)=>{
+  let userid=req.params.id;
+  await userModel.findByIdAndDelete(userid);
+  res.redirect("/read")
+})
 
+//listening to server
 app.listen("3000",()=>{
   console.log("app is running at localhot:3000")
 })
